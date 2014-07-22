ec90b6b4f33a1cddc1fe8b1ac9119a29ac3bf7f1
Tom Tunstill <tom.tunstill@worldpay.com>
2014-07-22 16:23:59 +0100

    cleanup

---
+++ javascripts/pageriser-demo.js
@@ -0,0 +1,25 @@
+(function ( $ ) {
+	//accessibility tweaks - make tabbing go through pages rather than individual items
+	$.fn.slickPages = function() {
+		var carousel = $(this);
+		carousel.find(".slick-cloned a").attr("tabindex", "-1");
+		carousel.find(".slick-slide").each(function(index,obj) {
+			var slide = $(obj);
+			slide.attr("data-slick-slideNum",index);
+			slide.find("a").focus(function(event) {
+				var focus = parseInt(slide.attr("data-slick-slideNum"), 10);
+				var pageSize = carousel.slickGetOption("slidesToShow")
+				var pos = focus - focus%pageSize;
+				carousel.slickGoTo(pos);
+			});
+		});
+		return carousel;
+	}
+	$("#theParent").arrange().slick({
+		accessiblity: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerPadding: '0',
+		infinite: false
+	}).slickPages();
+}( jQuery ));
